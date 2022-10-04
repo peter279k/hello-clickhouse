fb632345a1c1886c4ba7306eae6c016f047a39e0
peter <peter279k@gmail.com>
2022-10-04 07:30:22 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Hello ClickHouse
+
+# Prerequisites
+
+- Prepare the `java` command.
+- Prepare the `git` command.
+- Prepare the `mvn` command.
+
+# Usage
+
+- `mvn compile`
+- `mvn package`
+- `java -jar target/hello-clickhouse-0.1.0.jar`
---
+++ src/main/java/hello/HelloDB.java
@@ -0,0 +1,26 @@
+package hello;
+
+import java.sql.*;
+import java.util.*;
+import com.clickhouse.jdbc.*;
+
+public class HelloDB {
+	public static void main(String[] args) {
+            String url = "jdbc:ch:https://play.clickhouse.com:443";
+            Properties properties = new Properties();
+            properties.setProperty("user", "explorer");
+            properties.setProperty("password", "");
+            properties.setProperty("client_name", "Agent #1");
+
+            try {
+                ClickHouseDataSource dataSource = new ClickHouseDataSource(url, properties);
+                Connection conn = dataSource.getConnection();
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery("show databases");
+                System.out.println(rs);
+            } catch (SQLException e) {
+                 e.printStackTrace();
+                 System.out.println("Cannot connect the ClickHouse DB server");
+	    }
+	}
+}
